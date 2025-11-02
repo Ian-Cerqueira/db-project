82d041a7b08fb8d84b6a9c79b0163eeabc29bc9d
Ian-Cerqueira <iandavi16@outlook.com>
2025-11-02 20:43:30 -0300

    Adiciona delete Curtiu_Obra

--- pl-sql/sql_checklist.sql
+++ pl-sql/sql_checklist.sql
@@ -16,3 +16,15 @@ WHERE id = 3;
 UPDATE Lista_Personalizada
 SET visibilidade = '1'
 WHERE id = 1; 
+
+-- delete curtida de obra --
+DELETE FROM Curtiu_Obra
+WHERE id_usuario = 1, id_obra = 14;
+
+DELETE FROM Curtiu_Obra
+WHERE id_usuario = 3, id_obra = 1;
+
+DELETE FROM Curtiu_Obra
+WHERE id_usuario = 9, id_obra = 3;
+
+
